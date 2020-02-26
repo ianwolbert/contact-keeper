a891ce6bae19b386a7a8e8a002c752418c05cca8
Ian Wolbert <ian.wolbert@avian.com>
2020-02-25 20:44:15 -0500

    Fix loadUser

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
-import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
   return (
